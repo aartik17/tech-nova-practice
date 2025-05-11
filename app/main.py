95062a7e16ef3de60525a50be625d59b6233ee41
aartik17 <aartik.1704@gmail.com>
2025-05-11 23:50:31 +0530

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from login import login_bp
+from inventory import inventory_bp
+from report import report_bp
+
+app = Flask(__name__)
+app.register_blueprint(login_bp)
+app.register_blueprint(inventory_bp)
+app.register_blueprint(report_bp)
+
+@app.route('/')
+def home():
+    return "Welcome to TechNova Inventory Management System!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
